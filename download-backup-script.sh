f477451cb1e29774a808f246ea19f07776404777
thomas <thomaspelletier44@gmail.com>
2022-11-19 16:44:27 +0000

    first functionnal configuration

--- download-backup-script.sh
+++ download-backup-script.sh
@@ -6,6 +6,8 @@
 #
 ####
 
+RETENTION=10
+OLD=$(date +"%Y-%m-%d" -d "$RETENTION day ago")
 DATE=$(date +"%Y-%m-%d")
 HOST="glpi-ydays"
 LOCAL_DIR="/home/backup/"
@@ -19,3 +21,5 @@ scp -r $HOST:/tmp/$DATE/ /tmp/$DATE/
 tar -czf $LOCAL_DIR$DATE.tar.gz /tmp/$DATE/
 
 rm -rf /tmp/$DATE/
+
+rm $LOCAL_DIR$OLD.tar.gz
