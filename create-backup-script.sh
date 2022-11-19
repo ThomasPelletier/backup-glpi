5e33f603816be00728854526dc8aacf10e88d5d9
thomas <thomaspelletier44@gmail.com>
2022-11-19 15:10:29 +0000

    edit backup configuration

--- create-backup-script.sh
+++ create-backup-script.sh
@@ -3,10 +3,14 @@
 #### DESCRIPTION ####
 #
 #  This script allow you to create a dump file for glpi. You must run this script on the glpi server.
+#
+####
 
 #### REQUIREMENTS ####
 #
 # - mysqldump
+#
+####
 
 #### VARIABLE ####
 
@@ -17,15 +21,36 @@ source .env
 # DB_USER=
 # DB=
 # DB_PASSWD=
+#
+####
 
 DATE=$(date +"%Y-%m-%d")
 GLPI_DIR="/var/www/html/glpi/"
-BACKUP_DIR="/backup/"
 
 #### SCRIPT ####
 
-mkdir /tmp/$DATE/
+mkdir -p /tmp/$DATE/glpi
 
-cp -r $GLPI_DIR /tmp/$DATE/
+cp -r $GLPI_DIR* /tmp/$DATE/glpi
 
-mysqldump -u$DB_USER -p$DB_PASSWD -h localhost --database $DB > /tmp/$DATE/$DB.sql
+mysqldump -h 127.0.0.1 -u $DB_USER -p$DB_PASSWD --add-drop-database --hex-blob --dump-date --verbose --routines --triggers --events --quick --skip-lock-tables --column-statistics=0 $DB > /tmp/$DATE/$DB.sql
+
+#### IMPORT SQL BACKUP ####
+#
+# You have to create the glpi database before importing the backup :
+#
+# CREATE DATABASE <db_name>;
+#
+# Then, run the following command to import the backup :
+#
+# mysql -u <user> -h <host> -p <db_name> < /tmp/backup.sql
+#
+####
+
+#### IMPORT BACKUP ####
+#
+# cp -a /tmp/2022-11-19/glpi/* /var/www/html/glpi/
+#
+# chown -R www-data:www-data /var/www/html/glpi/
+#
+####
